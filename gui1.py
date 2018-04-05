e52e5311a3a28b68c974b59445ddbf88dea4e3e7
tharunShiv <tharun.rocky14@gmail.com>
2018-04-05 14:49:10 +0530

    frames done and packed

--- gui1.py
+++ gui1.py
@@ -1,6 +1,6 @@
-import tkinter
+import tkinter as tk
 
-root = tkinter.Tk()  # creating a root window
+root = tk.Tk()  # creating a root window
 
 '''TITLE of the window and the dimensions'''
 root.title("GUI Shiv")  
@@ -9,13 +9,18 @@ root.geometry("350x650")
 
 '''LABEL - used to display short pieces of text, which cannot be directly mutated
  by the user'''
-label = tkinter.Label(root, text='Mahadev!')
+label = tk.Label(root, text='Mahadev!')
 label.pack() # used to arrange the widget inside the parent(in this case-root) and
 # resize if necessary
 
 '''FRAMES - they are widgets that contain other widgets
 Not visible on the screen but are used to organize other widget'''
-
-
+f = tk.Frame(root) # the parent of the frame is the root
+custom_label = tk.Label(f, text='Sabapathi') #the parent of this label is f, not root
+custom_label_0 = tk.Label(f, text='Kabali') 
+# don't forget to pack it
+custom_label.pack()
+custom_label_0.pack()
+f.pack()
 
 root.mainloop() # start the main loop.
