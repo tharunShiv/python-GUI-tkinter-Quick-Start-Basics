ef3753419b4ac2a0a28449d27a8147b34760743c
tharunShiv <tharun.rocky14@gmail.com>
2018-04-05 15:04:04 +0530

    buttons added

---
+++ gui2.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('GUI Shiv')
+root.geometry('350x650')
+
+'''BUTTONS - you know it'''
+btn1 = tk.Button(root , text = "Click Me")
+btn2 = tk.Button(root , text = "Click Me not")
+# arrange the widgets in  grid fashion by specifying the row and column
+btn1.grid(row = 0, column = 1)
+btn2.grid(row = 0, column = 3)
+
+root.mainloop()
