1accebac4dfffb7c8c9bfc10284c1b031e22915a
tharunShiv <tharun.rocky14@gmail.com>
2018-04-05 14:43:32 +0530

    resized and added frames def

--- gui1.py
+++ gui1.py
@@ -2,16 +2,20 @@ import tkinter
 
 root = tkinter.Tk()  # creating a root window
 
-'''title of the window and the dimensions'''
+'''TITLE of the window and the dimensions'''
 root.title("GUI Shiv")  
-root.geometry("400x300")
+root.geometry("350x650")
 
 
-'''label - used to display short pieces of text, which cannot be directly mutated
+'''LABEL - used to display short pieces of text, which cannot be directly mutated
  by the user'''
-
 label = tkinter.Label(root, text='Mahadev!')
 label.pack() # used to arrange the widget inside the parent(in this case-root) and
 # resize if necessary
+
+'''FRAMES - they are widgets that contain other widgets
+Not visible on the screen but are used to organize other widget'''
+
+
 
 root.mainloop() # start the main loop.
